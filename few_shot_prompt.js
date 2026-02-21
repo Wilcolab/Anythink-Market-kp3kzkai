e4cfbcab8dd4371504fbad90a5a146e05c8ca011
Mahesh <143211836+Mahesh9teen@users.noreply.github.com>
2026-02-22 02:37:56 +0530

    Create few_shot_prompt.js
    @username Please review this PR.

---
+++ few_shot_prompt.js
@@ -0,0 +1,15 @@
+/*
+Input: [3, 1, 2]
+Output: [1, 2, 3]
+
+Input: [10, 5, 8]
+Output: [5, 8, 10]
+
+Input: [-1, 4, 0]
+Output: [-1, 0, 4]
+*/
+
+// Write a function sortArray(arr) that sorts an array in ascending order.
+function sortArray(arr) {
+
+}
